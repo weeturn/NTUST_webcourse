2c712912333f0ec4eb1af42f6663f3f80ed50756
weeturn <sasquatch110@gmail.com>
2020-12-13 22:11:33 +0800

    delete
    delete

--- 0522/web/cms/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import me
-# Register your models here.
-
-class meAdmin(admin.ModelAdmin):
-	list_display = ('name', 'phone_number', 'address')
-
-admin.site.register(me)
